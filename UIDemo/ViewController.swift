e7426f1aa85cf6f1ff005ae11faa516378f91a22
莫云江 <moyunjiang@s-MacBook-Pro.local>
2025-07-15 14:40:08 +0800

    init

---
+++ UIDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIDemo
+//
+//  Created by 莫云江 on 2025/7/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UIDemoTests/UIDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIDemoTests.swift
+//  UIDemoTests
+//
+//  Created by 莫云江 on 2025/7/15.
+//
+
+import Testing
+@testable import UIDemo
+
+struct UIDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
